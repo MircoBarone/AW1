3c11e5674182f88c79cb0f56149e42b03aaf850d
MircoBarone <s303526@studenti.polito.it>
2022-03-09 17:50:09 +0100

    Timeout

--- Esempioweek2/Timeout.js
+++ Esempioweek2/Timeout.js
@@ -9,3 +9,17 @@ setTimeout(ciao,2000,"Mirco");  //il tempo è espresso in mikllisecondi.
 //La funzione ciao viene messa in coda per eseguirsi dopo un certo tempo.
 //La funzione ritorna un id se volessi annullare il timer.
 //Qui l'evento è creato da me, tuttavia nel browser è causato dall'utente ad esempio prmendo il mouse
+
+
+//------------------INTERVAL-------------------------
+//Uso la funzione set interval, è come il set Timeout solo che è periodico
+
+const id=setInterval((tizio)=>{console.log(`Ciao ${tizio}`);},1000,"Mirco");
+
+//se non si fa nulla questo programma esegue in eterno, la puoi fermare solo da terminale
+//Nel browser non c'è nulla da fermare, si termina solo quando si chiude la pagina nel Browser
+
+
+
+setTimeout(()=>{clearInterval(id)},6000);  //termina la stampa--l'interval
+
