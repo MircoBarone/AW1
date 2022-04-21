b383cfc051d75fdb5db32728b214ddd22d2cdff7
MircoBarone <s303526@studenti.polito.it>
2022-04-22 00:31:00 +0200

    week7

--- EsempiWeek7/esempio1/src/ExamComponents.js
+++ EsempiWeek7/esempio1/src/ExamComponents.js
@@ -3,6 +3,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 //importo useState per poterlo utilizzare
 import {startTransition, useState} from "react";
 import React from 'react';
+import {Fragment} from "react";
 //partendo dall'esempio base voglio che i bottoni di cancellazione funzionino, non ha senso ragionare in termini di lista 
 //che ho definito, l'idea è mettere la lista in uno stato per poterla modificare al bisogno
 //e per poterne tenere il valore.
@@ -26,6 +27,7 @@ function ExamTable(props) {
   //i dati sono un array che ho passato come props
   //un girono i dati saranno forniti dal server
   const [esami,Setesami]=useState(props.exams);
+  const [visualizzo,setVisualizzo]=useState(0);
   //attenzione che quando definisci una callback devi farla specifica, non ha senso fare
   //semplice wrapping della funzione si set
   function cancellaEsame(id)
@@ -37,9 +39,14 @@ function ExamTable(props) {
 
 
   }
+  //Voglio inserire un form, dove lo metto, l'idea è aggiungere un bottone e fare comparire un form.
+  //Devo decidere dove far comparire il form.
+  //Il form noi lo mettiamo sotto Exam Table--compare con un bottone ed uno stato di servizio
+
   
   
   return(
+    <Fragment>
     <Table>
       <thead>
         <tr>
@@ -55,6 +62,14 @@ function ExamTable(props) {
         }
       </tbody>
     </Table>
+    {  (visualizzo==0) ? <Button onClick={(visualizzo)=>{setVisualizzo(1)}}>Add</Button>:
+        
+     
+     <ExamForm visualizzo={visualizzo} setVisualizzo={setVisualizzo}></ExamForm>
+      }
+    
+    
+    </Fragment>
   );
 }
 //voglio agire sullo stato dal bottone, lo faccio tramite la SetEsami dal bottone
@@ -80,5 +95,29 @@ function ExamActions(props) {
   onClick={()=>(props.cancellaesame(props.idesame))}
   ><i className='bi bi-trash3'></i></Button></td>
 }
+
+
+function ExamForm(props)
+{ let visualizzo=props.visualizzo;
+  let setVisualizzo=props.setVisualizzo;
+  const[name,setName]=useState("");
+  return (
+  //qua vorrei mettere un form, puoi usare un form di bootstrap, oppure i componenti html che hanno mapping sul DOm
+  //se fai cosi devi definire lo stile come bootstrap, oppure utilizzi bootstrap react.
+ 
+ <>
+  <form>
+  <input type="text" name="nome" className="form-control" placeholder="Inserisci un Titolo" value={name} onChange={(event)=>{setName(event.target.value.toUpperCase())}}></input>
+  </form>
+  <Button onClick={(visualizzo)=>{setVisualizzo(0)}}>Cancel</Button>  
+  </>
+)
+
+
+
+
+}
+
+
 
 export {ExamScores};
