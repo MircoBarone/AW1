196c2390e5d4d0aba0f625f81443ae8a00a2f05f
MircoBarone <s303526@studenti.polito.it>
2022-04-29 20:24:52 +0200

    Week8

--- EsempiWeek7/esempio1/src/ExamComponents.js
+++ EsempiWeek7/esempio1/src/ExamComponents.js
@@ -1,9 +1,11 @@
-import {Table, Button} from 'react-bootstrap';
+import {Table, Button, Form,Alert} from 'react-bootstrap';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 //importo useState per poterlo utilizzare
 import {startTransition, useState} from "react";
 import React from 'react';
 import {Fragment} from "react";
+import dayjs from "dayjs"
+
 //partendo dall'esempio base voglio che i bottoni di cancellazione funzionino, non ha senso ragionare in termini di lista 
 //che ho definito, l'idea è mettere la lista in uno stato per poterla modificare al bisogno
 //e per poterne tenere il valore.
@@ -39,6 +41,14 @@ function ExamTable(props) {
 
 
   }
+  function AddExam(e)
+  {   //L'oggetto che aggiungo deve essere nuovo ma lo creo da zero quindi ok.
+      Setesami((esami)=>{ return esami.concat(e)});
+      //Se lo stato dipende dal vecchio stato è utile anzi necessario adoperare una callback.
+
+
+
+  }
   //Voglio inserire un form, dove lo metto, l'idea è aggiungere un bottone e fare comparire un form.
   //Devo decidere dove far comparire il form.
   //Il form noi lo mettiamo sotto Exam Table--compare con un bottone ed uno stato di servizio
@@ -65,7 +75,7 @@ function ExamTable(props) {
     {  (visualizzo==0) ? <Button onClick={(visualizzo)=>{setVisualizzo(1)}}>Add</Button>:
         
      
-     <ExamForm visualizzo={visualizzo} setVisualizzo={setVisualizzo}></ExamForm>
+     <ExamForm visualizzo={visualizzo} setVisualizzo={setVisualizzo} AddExam={AddExam}></ExamForm>
       }
     
     
@@ -100,16 +110,63 @@ function ExamActions(props) {
 function ExamForm(props)
 { let visualizzo=props.visualizzo;
   let setVisualizzo=props.setVisualizzo;
+  let AddExam=props.AddExam;
   const[name,setName]=useState("");
+  const[code,setCode]=useState("");
+  const[voto,setVoto]=useState(18);
+  const[data,setData]=useState(dayjs());
+
+  const[errorMsg,seterrorMsg]=useState("");
+  
+  const handlerVoto=(event)=>{ const val=event.target.value;   setVoto(val);    }
+  
+  const handleSubmit=(event)=>{//L'idea è validare i campi e creare un nuovo esame da inserire nella lista
+  event.preventDefault();
+  if(voto>31)  {setVoto(()=>{return 31;}); seterrorMsg("Voto Riportato a 31"); return;  }
+  if(voto<18)  {setVoto(18);   seterrorMsg("Voto riportato a 18"); return;}
+  if(name==="") {seterrorMsg("Hai inserito un Titolo Vuoto"); return;}
+  if(code==="") {seterrorMsg("Hai inserito un codice vuoto"); return;}
+  if(data.diff(dayjs(),"d")>1) {seterrorMsg("Non puoi inserire un esame sostenuto nel futuro"); return;}
+  const newObjg={code: code, name: name, score: voto, date: data };
+  
+  AddExam(newObjg);
+  setVisualizzo(()=>(false));
+  
+  
+  
+  }
+
+
+
   return (
   //qua vorrei mettere un form, puoi usare un form di bootstrap, oppure i componenti html che hanno mapping sul DOm
   //se fai cosi devi definire lo stile come bootstrap, oppure utilizzi bootstrap react.
  
  <>
-  <form>
-  <input type="text" name="nome" className="form-control" placeholder="Inserisci un Titolo" value={name} onChange={(event)=>{setName(event.target.value.toUpperCase())}}></input>
-  </form>
-  <Button onClick={(visualizzo)=>{setVisualizzo(0)}}>Cancel</Button>  
+  
+  {(errorMsg!=="") ? <Alert variant="danger" onClose={()=>{seterrorMsg("")}} dismissible>{errorMsg}</Alert>:false}
+  <Form>
+  <Form.Group>
+  <Form.Label>Codice</Form.Label>
+   <Form.Control type="text" placeholder="Inserisci il Codice" value={code}   onChange={(event)=>{setCode(event.target.value)}}></Form.Control>
+   </Form.Group>
+   <Form.Group>
+   <Form.Label>Nome dell'esame</Form.Label>
+   <Form.Control type="text" placeholder="Inserisci il Titolo" value={name}   onChange={(event)=>{setName(event.target.value.toUpperCase())}}></Form.Control>
+  </Form.Group>
+  <Form.Group>
+   <Form.Label>Voto</Form.Label>
+   <Form.Control type="number" min={18} max={31} placeholder="Inserisci il Voto" value={voto}   onChange={(event)=>{ handlerVoto(event);}}></Form.Control>
+  </Form.Group>
+  <Form.Group>
+   <Form.Label>Data</Form.Label>
+   <Form.Control type="date"  placeholder="Inserisci la Data" value={data.format("YYYY-MM-DD")}   onChange={(event)=>{ setData(dayjs(event.target.value));}}></Form.Control>
+  </Form.Group>
+
+
+  <Button onClick={(visualizzo)=>{setVisualizzo(0)}}>Cancel</Button>
+  <Button onClick={handleSubmit}>Save</Button>
+  </Form> 
   </>
 )
 
